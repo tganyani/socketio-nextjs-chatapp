68ab48598236a0bd6ad67b09f19779de4768b58d
Tatenda Ganyani <tatendaganyani5@gmail.com>
2022-11-05 15:53:19 +0300

    second commit

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const httpServer = createServer(app)
 
 const io = new Server(httpServer, {
     cors: {
-        origin: "http://localhost:3000",
+        origin: "https://chat-frontf.herokuapp.com",
         methods:['GET','POST']
     }
 })
