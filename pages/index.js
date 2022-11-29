97082e2f776828faf2a5c30be417463d027aff6f
Tatenda Ganyani <tatendaganyani5@gmail.com>
2022-11-29 19:25:27 +0300

    added

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,6 @@ import { useSession, signIn, signOut } from "next-auth/react"
 import styles from '../styles/Home.module.scss'
 import Link from "next/link"
 
-<<<<<<< HEAD
 export default function Component() {
   const { data: session } = useSession()
 
@@ -24,46 +23,6 @@ export default function Component() {
        <button onClick={() => signIn()}>Sign in</button>
        <p>if you dont have an account</p>
        <Link href="/signup">Register</Link>
-=======
-const socket = io("https://mychat-a.herokuapp.com/")
-
-
-export default function Home() {
-  const [messages, setMessages] = useState([])
-  const [message, setMessage] = useState('')
-  useEffect(()=>{
-    // socket.on('greet',(data)=>{
-    //   setMessages(data)
-    //   console.log(data)
-    // })
-    socket.on('newmsg',(data)=>{
-      console.log(data)
-      setMessages(data)
-    })
-  },[])
-  const handleSubmit = async (e)=>{
-    e.preventDefault()
-    await socket.emit('newmessage',message)
-  }
-  return (
-    <div className={styles.container}>
-      <input
-        type="text"
-        onChange={e => setMessage(e.target.value)}
-      />
-      <button
-        type='submit'
-        onClick={handleSubmit}
-      >
-        send
-      </button>
-      <div>
-        {
-          messages.map((msg,i) => (
-            <p key={i}>{msg}</p>
-          ))
-        }
->>>>>>> 1b9007b42980aa19bcfa7bbdedaf383dda83bd3e
       </div>
     </div>
   )
